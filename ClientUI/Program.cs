0d110c0b3aff1e6bb1e6133b2741b3992af93d88
Raudel Ravelo Suarez <raudelravelo91@gmail.com>
2019-08-09 19:42:34 -0400

    Add .learningtransport to gitignore.

--- ClientUI/Program.cs
+++ ClientUI/Program.cs
@@ -22,6 +22,15 @@ namespace ClientUI
             var endpointConfiguration = new EndpointConfiguration("ClientUI");
 
             var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+    .ConfigureAwait(false);
+
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+
+            await endpointInstance.Stop()
+                .ConfigureAwait(false);
         }
     }
 }
