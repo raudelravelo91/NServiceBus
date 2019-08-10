e7bb72fbbf50cce72a21744c36a0296132a0ae65
Raudel Ravelo Suarez <raudelravelo91@gmail.com>
2019-08-09 23:08:36 -0400

    Add events.

--- Sales/PlaceOrderHandler.cs
+++ Sales/PlaceOrderHandler.cs
@@ -16,7 +16,13 @@ namespace Sales
         {
             log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
 
-            return Task.CompletedTask;
+            // This is normally where some business logic would occur
+
+            var orderPlaced = new OrderPlaced
+            {
+                OrderId = message.OrderId
+            };
+            return context.Publish(orderPlaced);
         }
     }
 }
