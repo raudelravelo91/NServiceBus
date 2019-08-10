d3783f3cac1b5a6ccd7ccf28c10736008fd5313c
Raudel Ravelo Suarez <raudelravelo91@gmail.com>
2019-08-09 20:03:31 -0400

    Add first handler.

---
+++ ClientUI/PlaceOrderHandler.cs
@@ -0,0 +1,22 @@
+﻿using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientUI
+{
+    public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
+    {
+        static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
+        public Task Handle(PlaceOrder message, IMessageHandlerContext context)
+        {
+            log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
